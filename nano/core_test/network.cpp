1d7355c199d0c5817e78d1965345b33dcdca1437
Guilherme Lawless <guilherme@nano.org>
2019-12-31 10:01:29 +0000

    Using a stamp since thread sleep may be more than expected

--- nano/core_test/network.cpp
+++ nano/core_test/network.cpp
@@ -881,8 +881,9 @@ TEST (bandwidth_limiter, validate)
 		nano::bandwidth_limiter limiter_40 (message_size * 2 * nano::bandwidth_limiter::buffer_size);
 
 		auto start (std::chrono::steady_clock::now ());
+		auto stamp = start;
 		bool dropped (false);
-		while (start + 3s > std::chrono::steady_clock::now ())
+		while (start + 5s > std::chrono::steady_clock::now ())
 		{
 			ASSERT_FALSE (limiter_0.should_drop (message_size)); // will never drop
 			ASSERT_TRUE (limiter_1.should_drop (message_size)); // always drop as message > limit / rate_buffer.size ()
@@ -890,7 +891,10 @@ TEST (bandwidth_limiter, validate)
 			ASSERT_FALSE (limiter_40.should_drop (message_size)); // should never drop
 			std::cout << dropped << " " << limiter_20.get_rate () << " " << limiter_20.get_limit () << " "
 			          << " -- " << limiter_40.get_rate () << " " << limiter_40.get_limit () << std::endl;
-			std::this_thread::sleep_for (27ms); // with a polling period of 50ms, this gets close to the limit of limiter_40
+			// With a polling period of 50ms, this gets close to the limit of limiter_40
+			auto sleep_time (std::max (26ms, std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::steady_clock::now () - stamp)));
+			stamp = std::chrono::steady_clock::now ();
+			std::this_thread::sleep_for (sleep_time);
 		}
 		ASSERT_TRUE (dropped);
 		ASSERT_EQ (limiter_0.get_rate (), 0); //should be 0 as rate is not gathered if not needed
