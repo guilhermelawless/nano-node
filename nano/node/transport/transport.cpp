467bda2c8ae63073fa815db045c2f942ce1e923a
Guilherme Lawless <guilherme@nano.org>
2019-12-31 08:27:29 +0000

    Fix calculations, normalization is done on the sampling period instead to account for fluctuations

--- nano/node/transport/transport.cpp
+++ nano/node/transport/transport.cpp
@@ -217,28 +217,33 @@ limit (limit_a)
 bool nano::bandwidth_limiter::should_drop (const size_t & message_size)
 {
 	bool result (false);
-	if (limit == 0) //never drop if limit is 0
+	// Never drop if limit is 0
+	if (limit == 0)
 	{
 		return result;
 	}
 	nano::lock_guard<std::mutex> lock (mutex);
-
+	auto now = std::chrono::steady_clock::now ();
+	if (next_trend < now)
+	{
+		rate_buffer.push_back (rate);
+		trended_rate = std::accumulate (rate_buffer.begin (), rate_buffer.end (), size_t{ 0 });
+		rate = 0;
+		// Reset if too much time has passed
+		if (now - next_trend > 2 * period)
+		{
+			next_trend = now;
+		}
+		// Increment to account for fluctuations in the sampling period
+		next_trend += period;
+	}
 	if (message_size > limit / rate_buffer.size () || trended_rate + message_size > limit)
 	{
 		result = true;
 	}
 	else
 	{
-		rate = rate + message_size;
-	}
-	auto now = std::chrono::steady_clock::now ();
-	if (next_trend < now)
-	{
-		// Normalize in case more time has passed
-		rate_buffer.push_back (rate * period / (now - next_trend + period));
-		trended_rate = std::accumulate (rate_buffer.begin (), rate_buffer.end (), size_t{ 0 });
-		rate = 0;
-		next_trend = now + period;
+		rate += message_size;
 	}
 	return result;
 }
@@ -247,4 +252,9 @@ size_t nano::bandwidth_limiter::get_rate ()
 {
 	nano::lock_guard<std::mutex> lock (mutex);
 	return trended_rate;
+}
+
+size_t nano::bandwidth_limiter::get_limit () const
+{
+	return limit;
 }
